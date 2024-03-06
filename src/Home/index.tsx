274634dfb968522e3223c32ee71e9134d589e0ce
Zhangtiancheng <2458015575@qq.com>
2024-03-06 15:40:31 +0800

    :art: 引入底部Tab

--- src/Home/index.tsx
+++ src/Home/index.tsx
@@ -1,15 +1,24 @@
 import React from 'react';
-import {useNavigation} from '@react-navigation/native';
+import {NavigationContainer, useNavigation} from '@react-navigation/native';
 import {Button, Text} from 'native-base';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import Message from './Message';
+import Contact from './Contact';
+
+const Tab = createBottomTabNavigator();
 
 export default function Home() {
   const navigation = useNavigation();
   return (
-    <>
-      <Text>主页</Text>
-      <Button onPress={() => navigation.navigate('Login' as never)}>
-        去Login
-      </Button>
-    </>
+    // <>
+    //   <Text>主页</Text>
+    //   <Button onPress={() => navigation.navigate('Login' as never)}>
+    //     去Login
+    //   </Button>
+    // </>
+    <Tab.Navigator>
+      <Tab.Screen name="message" component={Message} />
+      <Tab.Screen name="contact" component={Contact} />
+    </Tab.Navigator>
   );
 }
