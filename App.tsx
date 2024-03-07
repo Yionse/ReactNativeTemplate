4095c58bc8853965fa5eaea0ca4c975bd9dd5ecd
Zhangtiancheng <2458015575@qq.com>
2024-03-07 15:40:33 +0800

    :sparkles: 引入手势右滑Drawer

--- App.tsx
+++ App.tsx
@@ -1,16 +1,62 @@
-import React from 'react';
-import 'react-native-gesture-handler';
-import {NativeBaseProvider, Text} from 'native-base';
+import React, {useCallback, useRef, useState} from 'react';
+import {NativeBaseProvider, Text, View} from 'native-base';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import Login from './src/Login';
 import Home from './src/Home';
+import {Dimensions, PanResponder} from 'react-native';
 
 const Stack = createNativeStackNavigator();
 
 function Main() {
+  const swipeDistance = useRef<number>(0);
+  const swipeStartDistance = useRef<number>(0);
+  const [drawerLeftDistance, setDrawerLeftDistance] = useState<number>(0);
+  const drawerMove = useCallback(
+    (x: number) => {
+      setDrawerLeftDistance(
+        prevDistance => prevDistance + x - swipeDistance.current,
+      );
+    },
+    [drawerLeftDistance],
+  );
+  const panResponder = useRef(
+    PanResponder.create({
+      onStartShouldSetPanResponder: () => true,
+      onMoveShouldSetPanResponder: (evt, gestureState) => {
+        return (
+          gestureState.dx > 30 &&
+          Math.abs(gestureState.dy) < 10 &&
+          gestureState.vx > 0
+        );
+      },
+      onPanResponderMove: (evt, gestureState) => {
+        if (swipeDistance.current === 0) {
+          swipeStartDistance.current = gestureState.dx;
+        }
+        // 处理右滑动作的逻辑
+        drawerMove(gestureState.dx);
+        // setDrawerLeftDistance(
+        //   () => drawerLeftDistance + (gestureState.dx - swipeDistance.current),
+        // );
+        swipeDistance.current = gestureState.dx;
+      },
+      onPanResponderEnd: () => {
+        if (swipeDistance.current - swipeStartDistance.current > 200) {
+          setDrawerLeftDistance(300);
+        } else {
+          setDrawerLeftDistance(0);
+          swipeDistance.current = 0;
+          swipeStartDistance.current = 0;
+        }
+      },
+    }),
+  ).current;
   return (
-    <Stack.Navigator>
+    <Stack.Navigator
+      screenOptions={{
+        headerShown: false,
+      }}>
       <Stack.Screen name="Login" component={Login} />
       <Stack.Screen name="Home" component={Home} />
     </Stack.Navigator>
--- src/Home/index.tsx
+++ src/Home/index.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import {NavigationContainer, useNavigation} from '@react-navigation/native';
-import {Button, Text} from 'native-base';
+import React, {useCallback, useRef, useState} from 'react';
+import {Dimensions, PanResponder} from 'react-native';
+import {Text, View} from 'native-base';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import Message from './Message';
 import Contact from './Contact';
@@ -8,17 +8,69 @@ import Contact from './Contact';
 const Tab = createBottomTabNavigator();
 
 export default function Home() {
-  const navigation = useNavigation();
+  const swipeDistance = useRef<number>(0);
+  const swipeStartDistance = useRef<number>(0);
+  const [drawerLeftDistance, setDrawerLeftDistance] = useState<number>(0);
+  const drawerMove = useCallback(
+    (x: number) => {
+      setDrawerLeftDistance(
+        prevDistance => prevDistance + x - swipeDistance.current,
+      );
+    },
+    [drawerLeftDistance],
+  );
+  const panResponder = useRef(
+    PanResponder.create({
+      onStartShouldSetPanResponder: () => true,
+      onMoveShouldSetPanResponder: (evt, gestureState) => {
+        return (
+          gestureState.dx > 30 &&
+          Math.abs(gestureState.dy) < 10 &&
+          gestureState.vx > 0
+        );
+      },
+      onPanResponderMove: (evt, gestureState) => {
+        if (swipeDistance.current === 0) {
+          swipeStartDistance.current = gestureState.dx;
+        }
+        // 处理右滑动作的逻辑
+        drawerMove(gestureState.dx);
+        // setDrawerLeftDistance(
+        //   () => drawerLeftDistance + (gestureState.dx - swipeDistance.current),
+        // );
+        swipeDistance.current = gestureState.dx;
+      },
+      onPanResponderEnd: () => {
+        if (swipeDistance.current - swipeStartDistance.current > 200) {
+          setDrawerLeftDistance(300);
+        } else {
+          setDrawerLeftDistance(0);
+          swipeDistance.current = 0;
+          swipeStartDistance.current = 0;
+        }
+      },
+    }),
+  ).current;
   return (
-    // <>
-    //   <Text>主页</Text>
-    //   <Button onPress={() => navigation.navigate('Login' as never)}>
-    //     去Login
-    //   </Button>
-    // </>
-    <Tab.Navigator>
-      <Tab.Screen name="message" component={Message} />
-      <Tab.Screen name="contact" component={Contact} />
-    </Tab.Navigator>
+    <>
+      <View flex={1} {...panResponder.panHandlers} position={'relative'}>
+        <Tab.Navigator>
+          <Tab.Screen name="Message" component={Message} />
+          <Tab.Screen name="Contact" component={Contact} />
+        </Tab.Navigator>
+      </View>
+      <View
+        style={{
+          position: 'absolute',
+          left: 0,
+          top: 0,
+          transform: [
+            {translateX: -Dimensions.get('window').width + drawerLeftDistance},
+          ],
+          width: '100%',
+          height: Dimensions.get('window').height,
+          backgroundColor: 'skyblue',
+        }}></View>
+    </>
   );
 }
